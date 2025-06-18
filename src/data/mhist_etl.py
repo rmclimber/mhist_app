7ca9e6ef2cbf1617ca7cf278c5f04ac92f008d28
RM <rmorris.code@gmail.com>
2025-06-18 15:49:15 -0700

    ETL done for MHIST

--- src/data/mhist_etl.py
+++ src/data/mhist_etl.py
@@ -1,13 +1,15 @@
-import sys
 import os
 from pathlib import Path
 from argparse import ArgumentParser
 from collections.abc import Iterable
+from PIL import Image
+import pandas as pd
 import numpy as np
 
 class MHIST_ETL:
     BASE_FILENAME = "mhist_"
     REQUIRED = ["images", "annotations.csv"]
+    DIAG_MAP = {"SSA": 1, "HP": 0}
 
     def __init__(self, 
                  indir: str | Path, 
@@ -28,6 +30,10 @@ class MHIST_ETL:
         return all(req in present for req in self.REQUIRED)
 
     def _setup(self):
+        """
+        Validate the input directory as a real directory witih the necessary
+        structure, and create the output directory if it does not exist.
+        """
         # verify valid input directory
         if not self._validate_indir_structure():
             raise FileNotFoundError(
@@ -38,22 +44,82 @@ class MHIST_ETL:
         # make output directory
         try:
             os.makedirs(self.outdir, exist_ok=True)
+            print(f"Created output directory: {self.outdir}")
         except Exception as e:
             raise e
     
-    def _extract(self):
-        pass
+    def _extract(self) -> dict[str, list[np.ndarray | int]]:
+        """
+        Using pandas, open the annotations.csv and use it to build the images
+        and labels lists.
+        """
+        extracted = {"images": [], "labels": []}
+        annotations = pd.read_csv(self.indir / "annotations.csv")
 
-    def _transform(self):
-        pass
+        # iterate over the CSV, loading images and labels into lists for splitting
+        for _, row in annotations.iterrows():
+            image_name = annotations["Image Name"][row.name]
+            label = self.DIAG_MAP[annotations["Majority Vote Label"][row.name]]
+            with Image.open(self.indir / "images" / image_name) as image:
+                extracted["images"].append(np.array(image))
+            extracted["labels"].append(label)
+        return extracted
 
-    def _load(self):
-        pass
+    def _prep_indices(self, n: int) -> dict[str, list[int]]:
+        """
+        Use lists of indices to set up the trainn/val/test splits.
+        """
+        indices = np.arange(n)
+        self.rng.shuffle(indices)
+        train_stop = int(n * self.props[0])
+        val_stop = train_stop + int(n * self.props[1])
+        test_stop = val_stop + int(n * self.props[2])
+        return {"train": indices[:train_stop],
+                "val": indices[train_stop:val_stop],
+                "test": indices[val_stop:test_stop]}
+
+
+    def _transform(self, 
+                   extracted: dict[str, list]) -> dict[str, dict[str, np.ndarray]]:
+        """
+        Split the dataset.
+        """
+        n = len(extracted["images"])
+        indices = self._prep_indices(n)
+        transformed = {key: {"images": [], "labels": []} for key in indices.keys()}
+        for split in indices:
+            for idx in indices[split]:
+                transformed[split]["images"].append(extracted["images"][idx])
+                transformed[split]["labels"].append(extracted["labels"][idx])
+            transformed[split]["images"] = np.array(transformed[split]["images"])
+            transformed[split]["labels"] = np.array(transformed[split]["labels"])
+        return transformed
+
+    def _load(self, transformed: dict[str, dict[str, np.ndarray]]) -> None:
+        for split in transformed:
+            print(f"Saving {split} data...")
+
+            # set up paths and filenames
+            label_filename = self.BASE_FILENAME + split + "_labels.npy"
+            label_path = self.outdir / label_filename
+            image_filename = self.BASE_FILENAME + split + "_images.npy"
+            image_path = self.outdir / image_filename
+
+            # sanity checks, then save data
+            img_n = transformed[split]["images"].shape[0]
+            label_n = transformed[split]["labels"].shape[0]
+            assert img_n == label_n, "Images and labels do not match!"
+            np.save(label_path, transformed[split]["labels"])
+            np.save(image_path, transformed[split]["images"])
+            
+            print(f"Saved {img_n} images to {image_path}")
+            print(f"Saved {label_n} images to {label_path}")
+        print(f"Finished saving data to {self.outdir}")
 
     def run(self):
-        self._extract()
-        self._transform()
-        self._load()
+        extracted = self._extract()
+        transformed = self._transform(extracted)
+        self._load(transformed)
         print("In MHIST_ETL: run complete")       
 
 

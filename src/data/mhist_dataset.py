bc5b2eb73fe8c44201da218e166229fb2ba88601
RM <rmorris.code@gmail.com>
2025-06-20 07:59:00 -0700

    added normalization

--- src/data/mhist_dataset.py
+++ src/data/mhist_dataset.py
@@ -1,16 +1,23 @@
 import torch
 from torch.utils.data import Dataset
+from torchvision.transforms import Normalize
 import numpy as np
+
+from .data_info import *
 
 class MHISTDataset(Dataset):
     def __init__(self, 
                  img_filename: str, 
                  label_filename: str,
+                 data_info: MHISTDataInfo,
                  device: str = 'cpu'):
         super().__init__()
         self.img_filename = img_filename
         self.label_filename = label_filename
+        self.data_info = data_info
         self.device = device
+        self.normalize = Normalize(mean=self.data_info.mean, 
+                                   std=self.data_info.std)
         self._setup()
 
     def _setup(self):
@@ -21,7 +28,8 @@ class MHISTDataset(Dataset):
         return len(self.images)
     
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        return self.images[idx].to(self.device), self.labels[idx].to(self.device)
+        x, y = self.normalize(self.images[idx]), self.labels[idx]
+        return x.to(self.device), y.to(self.device)
 
 
         

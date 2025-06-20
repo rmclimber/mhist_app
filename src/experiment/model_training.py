fb4360f8e652fb09255badcd915d15977493912e
RM <rmorris.code@gmail.com>
2025-06-19 20:52:39 -0700

    model training skeleton in place

---
+++ src/experiment/model_training.py
@@ -0,0 +1,11 @@
+
+class MHISTTraining:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("run(): not implemented")
+
+if __name__ == '__main__':
+    mhist_training = MHISTTraining()
+    mhist_training.run()

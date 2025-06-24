a09a1beee9de33c705d4af1754402ef009eb3c17
RM <rmorris.code@gmail.com>
2025-06-23 23:10:48 -0700

    added MHISTDataConfig, fixed some mistakes

--- src/data/data_info.py
+++ src/data/data_info.py
@@ -1,7 +1,25 @@
 from dataclasses import dataclass, field
+from pathlib import Path
+import os
 
+@dataclass
+class MHISTDataConfig:
+    train_path: str | Path
+    val_path: str | Path
+    test_path: str | Path
+    num_workers: int = 4
+    batch_size: int = 32
+
+    def __post_init__(self):
+        self.train_path = Path(self.train_path)
+        self.val_path = Path(self.val_path)
+        self.test_path = Path(self.test_path)
+        self.num_workers = min(os.cpu_count(), self.num_workers)
+
+
+@dataclass
 class MHISTDataInfo:
-    label_map: dict = field(init=False)
+    label_map: dict
     num_classes: int = field(init=False)
     mean: list = field(init=False)
     std: list = field(init=False)

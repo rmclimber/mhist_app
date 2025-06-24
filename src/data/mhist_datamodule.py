db37405b1b5c554d707cf1a530bbd21a1f7c4ba2
RM <rmorris.code@gmail.com>
2025-06-23 23:10:26 -0700

    created the mhist datamodule, mostly done already

---
+++ src/data/mhist_datamodule.py
@@ -0,0 +1,36 @@
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader
+
+# my code
+from .mhist_dataset import *
+from .data_info import *
+
+class MHISTDataModule(LightningDataModule):
+    def __init__(self, 
+                 datasets: dict[str, MHISTDataset],
+                 data_config: MHISTDataConfig):
+        super().__init__()
+        self.datasets = datasets
+        self.data_config = data_config
+        self.setup()
+
+    def setup(self, stage=None):
+        self.train_dataset = self.datasets['train']
+        self.val_dataset = self.datasets['val']
+        self.test_dataset = self.datasets['test']
+    
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, 
+                          batch_size=self.data_config.batch_size,  
+                          num_workers=self.data_config.num_workers, 
+                          shuffle=True)
+    
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, 
+                          batch_size=self.data_config.batch_size,  
+                          num_workers=self.data_config.num_workers)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, 
+                          batch_size=self.data_config.batch_size,  
+                          num_workers=self.data_config.num_workers)

bafd95220c55b064cc9413b7b4e3c3ae07864d1a
RM <rmorris.code@gmail.com>
2025-06-25 15:56:46 -0700

    refactored MHISTDataInfo, added some other details to MHISTDataConfig

--- src/data/data_info.py
+++ src/data/data_info.py
@@ -7,6 +7,7 @@ class MHISTDataConfig:
     train_path: str | Path
     val_path: str | Path
     test_path: str | Path
+    info_path: str | Path
     num_workers: int = 4
     batch_size: int = 32
 
@@ -14,20 +15,22 @@ class MHISTDataConfig:
         self.train_path = Path(self.train_path)
         self.val_path = Path(self.val_path)
         self.test_path = Path(self.test_path)
+        self.info_path = Path(self.info_path)
         self.num_workers = min(os.cpu_count(), self.num_workers)
 
 
 @dataclass
 class MHISTDataInfo:
-    label_map: dict
+    info_dict: dict
+    label_map: dict = field(init=False)
     num_classes: int = field(init=False)
     mean: list = field(init=False)
     std: list = field(init=False)
-    stats: dict
 
     def __post_init__(self):
-        label_map = {int(k): v for k, v in self.label_map.items()}
+        label_map = self.info_dict["label_map"]
+        label_map = {int(k): v for k, v in label_map.items()}
         self.label_map = label_map
         self.num_classes = len(label_map)
-        self.mean = self.stats["mean"]
-        self.std = self.stats["std"]
+        self.mean = self.info_dict["stats"]["mean"]
+        self.std = self.info_dict["stats"]["std"]

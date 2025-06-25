be27e86046fa6482ea2b54e5037d7d082bb8e8b8
RM <rmorris.code@gmail.com>
2025-06-25 16:39:56 -0700

    a couple small changes: saving hyperparams, etc

--- src/model/lightning_vit.py
+++ src/model/lightning_vit.py
@@ -4,9 +4,14 @@ import torch
 import torch.nn as nn
 
 class LightningViT(LightningModule):
-    def __init__(self, num_classes, pretrained_model_name="google/vit-base-patch16-224"):
+    def __init__(self, 
+                 num_classes: int, 
+                 pretrained_model_name: str = "google/vit-base-patch16-224",
+                 lr: float = 1e-4):
         super().__init__()
+        self.save_hyperparameters()
         self.num_classes = num_classes
+        self.lr = lr
         self.config = ViTConfig.from_pretrained(pretrained_model_name)
         self.vit = ViTModel.from_pretrained(pretrained_model_name, config=self.config)
         self.classifier = nn.Linear(self.config.hidden_size, num_classes)
@@ -34,5 +39,5 @@ class LightningViT(LightningModule):
         return self.step(batch, "test")
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer

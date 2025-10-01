4528b4543674c78e6497eec15ea94bc1547d49ba
RM <rmorris.code@gmail.com>
2025-10-01 16:59:44 -0700

    added model and bucket info

--- src/app/backend/service_info.py
+++ src/app/backend/service_info.py
@@ -3,9 +3,18 @@ from dataclasses import dataclass, field
 
 @dataclass
 class ServiceInfo:
+    # container specifications
     class_service_url: str = field(init=False)
+    image_size: int = 224
     val_service_url: str = field(init=False)
     validate: bool = False
+
+    # model info
+    model_bucket: str = 'mhist-models'
+    class_model_path: str = "models/mhist_model.pth"
+    class_labels_path: str = "models/mhist_labels.json"
+    val_model_path: str = "models/mhist_val_model.pth"
+    val_labels_path: str = "models/mhist_val_labels.json"
 
     def __post_init__(self):
         self.class_service_url = os.getenv("CLASS_SERVICE_URL", "http://localhost:8000/classify")
